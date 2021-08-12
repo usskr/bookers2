2c5d3d9002d0b22f614db9b37613191cfad3a4a0
usskr <rei.takagi731@gmail.com>
2021-08-12 10:43:12 +0000

    [Finish] modelファイルにアソシエーションの記載

--- db/migrate/20210811113326_create_books.rb
+++ db/migrate/20210811113326_create_books.rb
@@ -4,6 +4,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
 
       t.string :title
       t.string :body
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_113326) do
+ActiveRecord::Schema.define(version: 2021_08_12_102545) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
