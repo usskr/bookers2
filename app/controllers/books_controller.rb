d7d90f66becc0db639e9083494b48033ccfa95fe
usskr <rei.takagi731@gmail.com>
2021-08-12 01:30:35 +0000

    [Finish] controllerファイルの作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,9 @@
+class BooksController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:new, :create]
+  resources :users, only: [:show]
 end
