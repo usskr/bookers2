cf730a88ad97da4fffd3fe46906820a8932d1934
usskr <rei.takagi731@gmail.com>
2021-08-13 05:35:56 +0000

    headerのレイアウト作成（途中）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#index'
   resources :books, only: [:index, :create, :show]
   resources :users, only: [:index, :create, :show, :edit]
 end
