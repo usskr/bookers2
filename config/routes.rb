f5b47693341c91bc803b1beb38203a0b2d41fe6c
usskr <rei.takagi731@gmail.com>
2021-08-16 11:14:13 +0000

    [Finish] bookのshowページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#index'
-  resources :books, only: [:index, :create, :show]
-  resources :users, only: [:index, :create, :show, :edit]
+  resources :books
+  resources :users
 end
