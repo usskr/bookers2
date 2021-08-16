fe3acba815af2ce3208f1b7cdabefdb6f3f32ceb
usskr <rei.takagi731@gmail.com>
2021-08-16 07:54:40 +0000

    [Finish] bookのindexページの右側の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,14 @@ class BooksController < ApplicationController
   def index
     @user = current_user
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     book = Book.new(book_params)
+    book.user_id = current_user.id
     book.save
-    redirect_to '/'
+    redirect_to books_path
   end
 
   def show
