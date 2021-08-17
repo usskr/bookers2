8a82e1564beb9948bb3e6fb6faf2aae5e91ea80b
usskr <rei.takagi731@gmail.com>
2021-08-17 08:34:20 +0000

    [Finish] フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,12 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: 'You have created book successfully.'
+    else
+      flash.now[:danger] = "create error"
+      render :index
+    end
   end
 
   def index
@@ -26,8 +30,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    else
+      flash.now[:danger] = "update error"
+      render :index
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,12 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
+    else  
+      flash.now[:danger] = "update error"
+      render :show
+    end
   end
   
   private
