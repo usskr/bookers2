1c2481e33e1f1436c9522681f7868b40eb81eeb9
usskr <rei.takagi731@gmail.com>
2021-08-17 05:24:26 +0000

    [Finish] userのshowページの作成　[Fix] UserInfo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,5 @@
 class BooksController < ApplicationController
 
-  def index
-    @user = current_user
-    @book = Book.new
-    @books = Book.all
-  end
-
   def create
     book = Book.new(book_params)
     book.user_id = current_user.id
@@ -13,23 +7,29 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
 
-  def show
+  def index
     @user = current_user
     @book = Book.new
-    @book_detail = Book.find(params[:id])
     @books = Book.all
   end
-  
+
+  def show
+    @book_detail = Book.find(params[:id])
+    @user = @book_detail.user
+    @book = Book.new
+    @books = Book.all
+  end
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
     redirect_to book_path(@book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
