e3d4e9f131dbb984bd6dbb837092f163778bc168
usskr <rei.takagi731@gmail.com>
2021-08-23 09:34:44 +0000

    [応用課題4] フォロー・フォロワー機能追加　コントローラの作成・編集、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#index'
-  resources :users
+  resources :users do
+    resource :relationships,only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
