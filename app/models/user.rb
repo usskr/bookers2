53a13b7e338a4716bcefecd84c2ba617be5c4444
usskr <rei.takagi731@gmail.com>
2021-08-23 08:15:30 +0000

    [応用課題4] フォロー・フォロワー機能追加　モデル作成、アソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: " Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, sources: :followed
+
   attachment :profile_image
 
   validates :name, uniqueness:true, length: { minimum:2, maximum: 20 }
