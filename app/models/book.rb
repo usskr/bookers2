474fabf338045a4977ec1c678a4b5a61ea82b858
usskr <rei.takagi731@gmail.com>
2021-08-24 11:06:39 +0000

    [応用課題5] 検索機能の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,18 @@ class Book < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
+  def self.looks(searches, words)
+    if searches == "perfect_match"
+      @book = Book.where("title LIKE ?", "#{words}")
+    elsif searches == "forward_match"
+      @book = Book.where("title LIKE ?", "#{words}%")
+    elsif searches == "backward_match"
+      @book = Book.where("title LIKE ?", "%#{words}")
+    else
+      @book = Book.where("title LIKE ?", "%#{words}%")
+    end
+  end
+
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
 
