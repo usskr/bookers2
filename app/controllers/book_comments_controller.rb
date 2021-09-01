082dd54e7fd03bac461e0eee3a1b39fc22c9946b
usskr <rei.takagi731@gmail.com>
2021-09-01 13:29:34 +0000

    [Fix] いいね・コメント機能の非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,18 +2,16 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book_comments = @book.book_comments
-    @book_comment = BookComment.new
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    comment.save
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    @book_comment.save
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    @book_comments = @book.book_comments
-    @book_comment = BookComment.new
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
   end
 
   private
