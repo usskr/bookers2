28d7d1e2bb9fe261c2739e94f62404c5ba6615f0
usskr <rei.takagi731@gmail.com>
2021-09-02 05:05:21 +0000

    [応用課題7a] 過去一週間でいいねの合計カウントが多い順に投稿を表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,9 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @user = current_user
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b| b.favorites.where(created_at: from...to).size <=> a.favorites.where(created_at: from...to).size}
   end
 
   def show
