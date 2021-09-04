67ac50837096a0ba730ace5da51558a086549fc7
usskr <rei.takagi731@gmail.com>
2021-09-04 14:38:23 +0000

    [応用課題8b] ユーザ同士で1対1のDM

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
 
   has_many :book_comments, dependent: :destroy
 
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
@@ -25,7 +28,7 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-  
+
   def self.search_for(content, method)
     if method == 'perfect'
       User.where(name: content)
