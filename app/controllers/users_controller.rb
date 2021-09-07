09f69ec34c019758b50f34e3c4c84aaedc3e5891
usskr <rei.takagi731@gmail.com>
2021-09-07 06:22:18 +0000

    [応用課題7b] ユーザ詳細ページに今週、先週の投稿数とその差を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,8 @@ class UsersController < ApplicationController
     @books = @user.books
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def edit
